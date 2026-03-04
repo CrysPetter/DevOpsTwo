d691d660f904a0fb50554c823080e612de708a25
CrysPetter <crystionpa@gmail.com>
2026-03-03 21:04:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Olá MUNDO Começamos com o Git"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"message": "Deu certo o teste nº 02"}
