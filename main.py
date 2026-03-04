ae01472940ef97b71ef2dbdf194d233a87d1f474
CrysPetter <crystionpa@gmail.com>
2026-03-03 21:09:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
+<<<<<<< Updated upstream
     return {"message": "Olá MUNDO Começamos com o Git"}
+=======
+    return {"message": "Olá MUNDO Começamos com o Git"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"message": "Deu certo o teste nº 02"}
+
+@app.get("/teste2")
+async def funcaoteste2():
+    return {"teste2": True, "num_aleatorio": random.randint(0, 1000)}
+>>>>>>> Stashed changes
